b42795f3df1002818f560cca265b5226b965aeda
Leonardo Corbi <leonardocorbi25@hotmail.com>
2024-01-17 14:13:24 -0300

    feat: display controll

--- src/Pages/Ola/index.tsx
+++ src/Pages/Ola/index.tsx
@@ -2,11 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Paragraph = styled.p`
-  &[data-test="true"] {
-    color: red;
+  .component-2, .component-1 {
+    display: none;
+  }
+
+  &[data-test="A"] {
+    .component-2 {
+      display: initial;
+    }
+
+    .component-1 {
+      display: none;
+    }
+  }
+
+  &[data-test="B"] {
+    .component-2 {
+      display: none;
+    }
+
+    .component-1 {
+      display: initial;
+    }
   }
 `;
 
-const Hello = () => <Paragraph>Olá</Paragraph>;
+const Hello = () => (
+  <Paragraph data-test="B">
+    <div className="component-1">Component 1</div>
+    <div className="component-2">Component 2</div>
+  </Paragraph>
+);
 
 export default Hello;
